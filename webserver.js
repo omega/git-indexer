89416a55b4783e7d8317c0aa3ffbe532b65f3787
Andreas Marienborg <andreas.marienborg@gmail.com>
2011-08-08 10:26:44 +0700

    Make the web server show the log buffer for /log

--- webserver.js
+++ webserver.js
@@ -23,29 +23,50 @@ WebServer.prototype.start = function() {
     http.createServer(function(req, resp) {
         logger.log("->WebServer: ".cyan + req.url);
         var r = url.parse(req.url, true);
-        resp.writeHead(200, {"Content-Type": "application/json"});
-        if (!r.query.issue) {
-            // No issue specified, lets return empty
-            resp.write(JSON.stringify({ 'error': 'No issue specified' }));
-            resp.end();
-            return;
+        if (r.pathname == '/log') {
+            self.handle_log(r, resp);
+        } else {
+            self.handle_issue(r, resp);
         }
-        Issue.findOne({'key': r.query.issue}, function(err, issue) {
-            if (err) logger.error(err);
-
-            if (issue) {
-                // Should re-sort the events
-                issue.events = issue.events.sort(function(a, b) {
-                    if (!b.date || !a.date) return 0; // undetermined
-                    return a.date.getTime() - b.date.getTime();
-                });
-                resp.write(JSON.stringify(issue));
-            }
-            if (!issue) resp.write(JSON.stringify({'error': "No issue found with that key"}));
-            resp.end();
-        });
     }).listen(self.port);
     logger.log("WebServer".cyan, "started, listening on http://localhost:" + this.port + "/");
+};
+
+WebServer.prototype.handle_log = function(r, resp) {
+    resp.writeHead(200, {"Content-Type": "text/html;charset=utf-8"});
+    resp.write("<html><head><title>git-indexer log</title>");
+    resp.write("<style>.error { color: red; } .warn { color: orange; }</style></head>");
+    resp.write("<body><ul>");
+    logger.buffer.forEach(function(v) {
+        resp.write("<li class='" + v.level + "'>" +
+            v.args.join(" ").replace("<", "&lt;").replace(">", "&gt;") +
+            "</li>");
+    });
+    resp.write('</ul></body></html>');
+    resp.end();
+};
+WebServer.prototype.handle_issue = function(r, resp) {
+    resp.writeHead(200, {"Content-Type": "application/json"});
+    if (!r.query.issue) {
+        // No issue specified, lets return empty
+        resp.write(JSON.stringify({ 'error': 'No issue specified' }));
+        resp.end();
+        return;
+    }
+    Issue.findOne({'key': r.query.issue}, function(err, issue) {
+        if (err) logger.error(err);
+
+        if (issue) {
+            // Should re-sort the events
+            issue.events = issue.events.sort(function(a, b) {
+                if (!b.date || !a.date) return 0; // undetermined
+                return a.date.getTime() - b.date.getTime();
+            });
+            resp.write(JSON.stringify(issue));
+        }
+        if (!issue) resp.write(JSON.stringify({'error': "No issue found with that key"}));
+        resp.end();
+    });
 };
 
 module.exports = WebServer;
