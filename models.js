376fc3b48d376e6ab5094c4c973744687a3a0da8
Andreas Marienborg <andreas.marienborg@gmail.com>
2013-03-28 14:50:56 +0700

    parse the number of commits to int

--- models.js
+++ models.js
@@ -187,7 +187,7 @@ function defineModels() {
                         return cb("Wrong branch?");
                     }
                     var m = stdout.match(/^(.*)-(\d+)-([a-z0-9]+)\n$/);
-                    cb(err, m[1], m[2], m[3]);
+                    cb(err, m[1], parseInt(m[2]), m[3]);
                 }
                 );
         });
